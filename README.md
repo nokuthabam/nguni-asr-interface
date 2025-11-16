574ec9544a40a3e420c3b52fbd5a179f94422537
Nokuthaba <nokuthaba.m@outlook.com>
2025-11-16 13:59:38 +0200

    Create Interface
    - Added support for wav2vec2 monolingual models

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# nguni-asr-interface
-Interface to Test Nguni ASR Models
+# Nguni ASR Demo
+Interface to Test Nguni ASR Models finetuned using whisper and wav2vec2
+
+A multilingual Nguni ASR interface supporting:
+- isiZulu (zu_wav2vec2)
+- isiXhosa (zu_wav2vec2)
+- siSwati (ssw_wav2vec2)
+- Multilingual Nguni model (nguni_wav2vec2)
+
+This space loads models directly from Hugging Face Hub using 'pipeline()'
+
+
+## Features
+- Microphone recording
+- Audio upload
+- Dynamic model selection
+- Runs on CPU inside Hugging Face Spaces
+
+## How to use
+1. Select a language/model
+2. Record or upload audio
+3. Receive transcription
---
+++ app.py
@@ -0,0 +1,34 @@
+import gradio as gr
+from transformers import pipeline
+
+models = {
+    "isiZulu": "nmoyo45/zu_wav2vec2",
+    "isiXhosa": "nmoyo45/xh_wav2vec2",
+    "siSwati": "nmoyo45/sv_wav2vec2",
+    "isiNdebele": "nmoyo45/nbl_wav2vec2",
+}
+
+
+def transcribe_audio(language, audio):
+    """
+    Transcribe audio using the specified model.
+    """
+
+    model_id = models[language]
+    asr = pipeline("automatic-speech-recognition", model=model_id)
+    transcription = asr(audio)["text"]
+    return transcription
+
+demo = gr.Interface(
+    fn =transcribe_audio,
+    inputs=[
+        gr.Dropdown(choices=list(models.keys()), label="Select Language"),
+        gr.Audio(sources=["upload", "microphone"], type="filepath", label="Record or Upload Audio")
+    ],
+    outputs = "textbox",
+    title="Multilingual Speech-to-Text Transcription",
+    description="Transcribe audio in isiZulu, isiXhosa, siSwati, and isiNdebele using pre-trained models."
+)
+
+if __name__ == "__main__":
+    demo.launch()
